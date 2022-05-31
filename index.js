3e9bd7279b1177309d78504fb4e71b27b672c617
Yeray Lopez <ylopezjordano@gmail.com>
2022-05-31 16:54:28 +0200

    js-test finished

--- index.js
+++ index.js
@@ -1,3 +1,193 @@
-const drawContainer = (containerSize, childSize, numberOfChildren) => {};
+// Function to generate random color
+const getRandomColor = () => {
+    const CSS_COLOR_NAMES = [
+        "AliceBlue",
+        "AntiqueWhite",
+        "Aqua",
+        "Aquamarine",
+        "Azure",
+        "Beige",
+        "Bisque",
+        "Black",
+        "BlanchedAlmond",
+        "Blue",
+        "BlueViolet",
+        "Brown",
+        "BurlyWood",
+        "CadetBlue",
+        "Chartreuse",
+        "Chocolate",
+        "Coral",
+        "CornflowerBlue",
+        "Cornsilk",
+        "Crimson",
+        "Cyan",
+        "DarkBlue",
+        "DarkCyan",
+        "DarkGoldenRod",
+        "DarkGray",
+        "DarkGrey",
+        "DarkGreen",
+        "DarkKhaki",
+        "DarkMagenta",
+        "DarkOliveGreen",
+        "DarkOrange",
+        "DarkOrchid",
+        "DarkRed",
+        "DarkSalmon",
+        "DarkSeaGreen",
+        "DarkSlateBlue",
+        "DarkSlateGray",
+        "DarkSlateGrey",
+        "DarkTurquoise",
+        "DarkViolet",
+        "DeepPink",
+        "DeepSkyBlue",
+        "DimGray",
+        "DimGrey",
+        "DodgerBlue",
+        "FireBrick",
+        "FloralWhite",
+        "ForestGreen",
+        "Fuchsia",
+        "Gainsboro",
+        "GhostWhite",
+        "Gold",
+        "GoldenRod",
+        "Gray",
+        "Grey",
+        "Green",
+        "GreenYellow",
+        "HoneyDew",
+        "HotPink",
+        "IndianRed",
+        "Indigo",
+        "Ivory",
+        "Khaki",
+        "Lavender",
+        "LavenderBlush",
+        "LawnGreen",
+        "LemonChiffon",
+        "LightBlue",
+        "LightCoral",
+        "LightCyan",
+        "LightGoldenRodYellow",
+        "LightGray",
+        "LightGrey",
+        "LightGreen",
+        "LightPink",
+        "LightSalmon",
+        "LightSeaGreen",
+        "LightSkyBlue",
+        "LightSlateGray",
+        "LightSlateGrey",
+        "LightSteelBlue",
+        "LightYellow",
+        "Lime",
+        "LimeGreen",
+        "Linen",
+        "Magenta",
+        "Maroon",
+        "MediumAquaMarine",
+        "MediumBlue",
+        "MediumOrchid",
+        "MediumPurple",
+        "MediumSeaGreen",
+        "MediumSlateBlue",
+        "MediumSpringGreen",
+        "MediumTurquoise",
+        "MediumVioletRed",
+        "MidnightBlue",
+        "MintCream",
+        "MistyRose",
+        "Moccasin",
+        "NavajoWhite",
+        "Navy",
+        "OldLace",
+        "Olive",
+        "OliveDrab",
+        "Orange",
+        "OrangeRed",
+        "Orchid",
+        "PaleGoldenRod",
+        "PaleGreen",
+        "PaleTurquoise",
+        "PaleVioletRed",
+        "PapayaWhip",
+        "PeachPuff",
+        "Peru",
+        "Pink",
+        "Plum",
+        "PowderBlue",
+        "Purple",
+        "RebeccaPurple",
+        "Red",
+        "RosyBrown",
+        "RoyalBlue",
+        "SaddleBrown",
+        "Salmon",
+        "SandyBrown",
+        "SeaGreen",
+        "SeaShell",
+        "Sienna",
+        "Silver",
+        "SkyBlue",
+        "SlateBlue",
+        "SlateGray",
+        "SlateGrey",
+        "Snow",
+        "SpringGreen",
+        "SteelBlue",
+        "Tan",
+        "Teal",
+        "Thistle",
+        "Tomato",
+        "Turquoise",
+        "Violet",
+        "Wheat",
+        "White",
+        "WhiteSmoke",
+        "Yellow",
+        "YellowGreen",
+      ];
+      return CSS_COLOR_NAMES[Math.round(Math.random() * 147)];
+}
 
-drawContainer(200, 50, 17);
+const drawContainer = (containerSize, childSize, numberOfChildren) => {
+    let canvas = document.getElementById('mainSquare');
+    let container = document.createElement('div');
+    let maxPossibleChilds = (Math.trunc(containerSize/childSize) * (Math.trunc(containerSize/childSize)));
+    // Set the maximum amount of childs
+    let maxChilds = Math.min(maxPossibleChilds, numberOfChildren);
+    container.style.width = containerSize+'px';
+    container.style.height = containerSize+'px';
+    container.style.display = 'flex';
+    container.style.flexDirection = 'row';
+    container.style.flexWrap = 'wrap';
+    canvas.appendChild(container);
+    let children = 0;
+    for (children;children <= maxChilds - 1; children++) {
+        let divChild = document.createElement('div');
+        divChild.style.width = childSize+'px';
+        divChild.style.height = childSize+'px';
+        divChild.style.backgroundColor = getRandomColor();
+        let timeoutID = null;
+        // Add event to start the delete timer and change the background when mouse enters
+        divChild.addEventListener('mouseover', function() {
+            divChild.style.backgroundColor = getRandomColor();
+            timeoutID = window.setTimeout(function(){
+                divChild.remove();
+            }, 2000);
+        })
+        // Clear timeout when the mouse leaves the div
+        divChild.addEventListener('mouseout', function() {
+            window.clearTimeout(timeoutID);
+        })
+        container.appendChild(divChild);
+    }
+    if (maxChilds !== numberOfChildren) {
+        alert('Only ' + maxChilds + ' children have been created')
+    }
+}
+
+drawContainer(432, 40, 400);
